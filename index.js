1eb940f7557d42fb47b9e7c10f828b0050b62903
victoriamaccari <maccaricampos@gmail.com>
2020-11-05 21:06:55 +0100

    2 files modified

--- index.js
+++ index.js
@@ -43,3 +43,31 @@ var visualization = d3plus.viz()
     .y("value")
     .axes({"ticks" : false})
     .draw()
+
+  var data = [
+    {"year": 1991, "name":"alpha", "value": 15},
+    {"year": 1992, "name":"alpha", "value": 34},
+    {"year": 1991, "name":"alpha2", "value": 17},
+    {"year": 1992, "name":"alpha2", "value": 65},
+    {"year": 1991, "name":"beta", "value": 10},
+    {"year": 1992, "name":"beta", "value": 10},
+    {"year": 1991, "name":"beta2", "value": 40},
+    {"year": 1992, "name":"beta2", "value": 38},
+    {"year": 1991, "name":"gamma", "value": 5},
+    {"year": 1992, "name":"gamma", "value": 10},
+    {"year": 1991, "name":"gamma2", "value": 20},
+    {"year": 1992, "name":"gamma2", "value": 34},
+    {"year": 1991, "name":"delta", "value": 50},
+    {"year": 1992, "name":"delta", "value": 43},
+    {"year": 1991, "name":"delta2", "value": 17},
+    {"year": 1992, "name":"delta2", "value": 35}
+  ]
+  var visualization = d3plus.viz()
+    .container("#viz3")
+    .data(data)
+    .type("box")
+    .id("name")
+    .x("year")
+    .y("value")
+    .axes({"ticks" : false})
+    .draw()
